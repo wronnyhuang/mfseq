608036a1d09653cc8224e1ee0022a5d47c9b0b9c
Ronny <wrhuang@mit.edu>
2019-02-28 23:56:36 -0500

    full matrix as input

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ parser.add_argument('-trancoef', default=10, type=float)
 parser.add_argument('-wdeccoef', default=5e-5, type=float)
 parser.add_argument('-nnegcoef', default=1e-1, type=float)
 parser.add_argument('-maxgradnorm', default=10, type=float)
-parser.add_argument('-nepoc', default=1, type=int)
+parser.add_argument('-nepoc', default=100, type=int)
 parser.add_argument('-logtrain', default=2, type=int)
 parser.add_argument('-logtest', default=5, type=int)
 parser.add_argument('-logimage', action='store_true')
@@ -46,23 +46,14 @@ class Model():
   def build_graph(self):
     '''build computational graph'''
 
-    with tf.name_scope('hyperparams'):
-      self.lr = tf.placeholder(tf.float32, [], name='lr')
-      self.trancoef = tf.placeholder(tf.float32, [], name='trancoef')
-      self.wdeccoef = tf.placeholder(tf.float32, [], name='wdeccoef')
-      self.nnegcoef = tf.placeholder(tf.float32, [], name='nnegcoef')
-
     with tf.name_scope('inputs'):
-      self.timeidx = tf.placeholder(tf.int32, [None], name='timeidx')
-      self.nodeidx = tf.placeholder(tf.int32, [None], name='nodeidx')
-      self.featidx = tf.placeholder(tf.int32, [None], name='featidx')
-      self.labels = tf.placeholder(tf.float32, [None], name='labels')
+      self.lr = tf.placeholder(tf.float32, [], name='lr')
+      self.X_true = tf.placeholder(tf.float32, datacube.shape, name='labels')
+      self.gategrad = tf.placeholder(tf.float32, datacube.shape, name='labels')
 
     with tf.name_scope('forward'):
-
       self.H = tf.Variable(tf.random_gamma(shape=[args.rank, nfeat], alpha=1.0), name='H', trainable=False)
       self.T = tf.Variable(tf.random_gamma(shape=[args.rank, args.rank], alpha=1.0), name='T', trainable=False)
-
       X = []
       self.W = []
       self.W_tran = []
@@ -73,18 +64,11 @@ class Model():
         if t > 0: # add transition regularizer
           w_tran = tf.matmul(self.W[-2], self.T, name='W_tran_'+str(t))
           self.W_tran.append(w_tran)
-
-    with tf.name_scope('predictions'):
       X = tf.stack(X, axis=0, name='X')
-      Xflat = tf.reshape(X, [-1], name='Xflat')
-      with tf.name_scope('indices_of_datapoints'):
-        indices = self.timeidx*nnode*nfeat + self.nodeidx*nfeat + self.featidx
-      self.preds = tf.gather(Xflat, indices, name='Xgather')
 
-    with tf.name_scope('loss'):
-      criterion = tf.losses.mean_squared_error
-      with tf.name_scope('criterion'):
-        self.crit = criterion(self.preds, self.labels)
+    criterion = tf.losses.mean_squared_error
+
+    with tf.name_scope('regularizers'):
       with tf.name_scope('transition'):
         self.tran = tf.add_n([criterion(w_tran, w) for w_tran, w in zip(self.W_tran, self.W[1:])]) / len(self.W_tran)
       with tf.name_scope('weight_decay'):
@@ -92,20 +76,21 @@ class Model():
       with tf.name_scope('nonneg'):
         self.nneg = tf.global_norm([tf.nn.relu(-t) for t in tf.trainable_variables()])**2
 
-      # linearly combine all the losses into the final training objective
-      self.loss = tf.add_n([self.wdeccoef * self.wdec, self.nnegcoef * self.nneg], name='loss')
+    with tf.name_scope('loss'):
+      self.crit = criterion(X, self.X_true)
+      self.loss = tf.add_n([self.crit, args.wdeccoef * self.wdec, args.nnegcoef * self.nneg], name='loss')
 
-    with tf.name_scope('trainop'):
+    with tf.name_scope('train_ops'):
       # keep track of training step
       self.step = tf.train.get_or_create_global_step()
       # clip gradients by a max norm value
       opt = tf.train.AdamOptimizer(self.lr)
       grads = tf.gradients(self.loss, tf.trainable_variables())
-      self.grads = grads
-      grads, self.gradnorm = tf.clip_by_global_norm(grads, args.maxgradnorm)
+      # grads, self.gradnorm = tf.clip_by_global_norm(grads, args.maxgradnorm)
       # training op
       self.trainop = opt.apply_gradients(zip(grads, tf.trainable_variables()), global_step=self.step, name='trainop')
 
+    # tensorboard summaries
     tf.summary.scalar('train/crit', self.crit)
     tf.summary.scalar('train/loss', self.loss)
     tf.summary.scalar('lr', self.lr)
@@ -120,55 +105,30 @@ class Model():
     self.writer = tf.summary.FileWriter(self.args.logdir, graph=self.sess.graph)
 
 
-  def test(self, testloader, step):
+  def test(self, step):
     '''test on entire test set'''
-
-    # test over all test data
-    running_crit = 0
-    for i, batch in enumerate(testloader):
-      timeidx, nodeidx, featidx, labels = batch.numpy().transpose()
-      crit, = self.sess.run([self.crit],
-                            {self.timeidx: timeidx,
-                             self.nodeidx: nodeidx,
-                             self.featidx: featidx,
-                             self.labels: labels,
-                             })
-      running_crit += crit * len(batch)
-    avg_crit = running_crit / len(testloader.dataset)
-    experiment.log_metric('test/crit', avg_crit, step)
-    print('TEST:\tstep', step, '\tcrit', avg_crit)
+    crit = self.sess.run(self.crit, {self.X_true: datacube})
+    experiment.log_metric('test/crit', crit, step)
+    print('TEST:\tstep', step, '\tcrit', crit)
 
 
-  def fit(self, trainloader, testloader):
+  def fit(self):
     '''fit the model to the data presented in the input dataloader'''
-
     step = 0
-    self.metrics = dict(loss=self.loss, crit=self.crit, tran=self.tran, wdec=self.wdec, nneg=self.nneg, gradnorm=self.gradnorm, grads=self.grads)
+    self.metrics = dict(loss=self.loss, crit=self.crit, tran=self.tran, wdec=self.wdec, nneg=self.nneg, gradnorm=self.gradnorm)
     for epoch in range(args.nepoc):
-
-      # train for an epoch
-      for i, batch in enumerate(trainloader):
-
-        dropfactor = 0.1 if step > args.lrdrop else 1
-        warmfactor = np.clip(step/5, 0, 1)
-        timeidx, nodeidx, featidx, labels = batch.numpy().transpose()
-        _, metrics, step, = self.sess.run([self.trainop, self.metrics, self.step],
-                           {self.timeidx: timeidx,
-                            self.nodeidx: nodeidx,
-                            self.featidx: featidx,
-                            self.labels: labels,
-                            self.lr: args.lrnrate * dropfactor * warmfactor,
-                            self.trancoef: args.trancoef,
-                            self.wdeccoef: args.wdeccoef,
-                            self.nnegcoef: args.nnegcoef,
-                            })
-        if np.mod(step, args.logtrain)==0:
-          experiment.log_metrics(metrics, step=step)
-          print('TRAIN:\tepoch', epoch, '\tstep', step, '\tcrit', metrics['crit'], '\tloss', metrics['loss'])
-        if np.mod(step, args.logtest)==0:
-          self.test(testloader, step)
-          if args.logimage: self.plot()
-
+      dropfactor = 0.1 if step > args.lrdrop else 1
+      _, metrics, step, = self.sess.run([self.trainop, self.metrics, self.step],
+                                        {self.X_true: datacube,
+                                         self.lr: args.lrnrate * dropfactor,
+                                         self.gategrad: gate,
+                                        })
+      if np.mod(step, args.logtrain)==0:
+        experiment.log_metrics(metrics, step=step)
+        print('TRAIN:\tepoch', epoch, '\tstep', step, '\tcrit', metrics['crit'], '\tloss', metrics['loss'])
+      if np.mod(step, args.logtest)==0:
+        self.test(step)
+        if args.logimage: self.plot()
     print('done training')
 
 
@@ -178,8 +138,6 @@ class Model():
 
   def plot(self, ending=False):
     '''plot and save distributions'''
-
-    # distribution of parameters
     params = self.get_params()
     T, H, W = params['T'], params['H'], params['W']
     for i, w in enumerate(W):
@@ -199,29 +157,13 @@ class Model():
     if ending:
 
       # plot distribution of labels
-      hist(labels, 200); xlim(-.1, 2); title('distribution-labels')
+      hist(datacube.ravel()[np.abs(datacube.ravel())>1e-2].ravel(), 200); xlim(-.1, 2); title('distribution-labels')
       experiment.log_figure(figure=plt.gcf())
 
       # dump W, H, T to disk
       with open(join(args.logdir, 'learned_params.joblib'), 'wb') as f:
         joblib.dump(params, f)
         experiment.log_asset(join(args.logdir, 'learned_params.joblib'))
-
-
-class GraphDataset(torch.utils.data.Dataset):
-  '''dataset object for the aml graph data with features extracted via refex'''
-
-  def __init__(self, timeidx, nodeidx, featidx, labels, train=True):
-
-    self.data = np.array(list((zip(timeidx, nodeidx, featidx, labels))))
-    condition = timeidx != timeidx.max() if train else timeidx == timeidx.max()
-    self.data = self.data[condition]
-
-  def __len__(self):
-    return len(self.data)
-
-  def __getitem__(self, idx):
-    return self.data[idx]
 
 
 if __name__=='__main__':
@@ -240,20 +182,22 @@ if __name__=='__main__':
   os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
 
   # load data from file
-  maybe_download('https://www.dropbox.com/s/cdu4xx8vnavkvz3/graph_data_table.pkl?dl=0',
-                 'graph_data_table.pkl', join(home, 'datasets'), filetype='file')
-  with gzip.open(join(home, 'datasets', 'graph_data_table.pkl'), 'rb') as f:
-    (timeidx, nodeidx, featidx), labels, (ntime, nnode, nfeat) = pickle.load(f)
+  maybe_download('https://www.dropbox.com/s/lu38zp3ixjpth9e/graph_data_cube.pkl?dl=0',
+                 'graph_data_cube.pkl', join(home, 'datasets'), filetype='file')
+  with gzip.open(join(home, 'datasets', 'graph_data_cube.pkl'), 'rb') as f:
+    datacube = pickle.load(f)
 
-  # create dataloader object
-  trainloader = torch.utils.data.DataLoader(GraphDataset(timeidx, nodeidx, featidx, labels, True), batch_size=args.batchsize, shuffle=True, num_workers=0)
-  testloader = torch.utils.data.DataLoader(GraphDataset(timeidx, nodeidx, featidx, labels, False), batch_size=args.batchsize, shuffle=False, num_workers=0)
+  ntime, nnode, nfeat = datacube.shape
+  gate = np.ones([ntime, nnode, args.rank]) # gate is multiplied against gradients wrt W
+  nodeidx = np.random.permutation(nnode)
+  split = int(.9*len(nodeidx))
+  gate[:, nodeidx[split:], :] = 0
 
   # build tf graph
   model = Model(args)
 
   # run optimizer on training data
-  model.fit(trainloader, testloader)
+  model.fit()
 
   # plot stuff
   model.plot(ending=True)
